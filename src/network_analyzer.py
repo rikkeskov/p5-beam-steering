65d9232e260f3ccaaf4efca5ef742dfc53d7d3e2
Rikke <rudeng20@student.aau.dk>
2024-04-15 21:13:59 +0200

    15/4: design section about turntable software

--- src/network_analyzer.py
+++ src/network_analyzer.py
@@ -8,6 +8,7 @@
 """
 
 import logging
+import numpy
 from rohdeschwarz.instruments.vna import Vna
 logger = logging.getLogger(__name__)
 
@@ -23,15 +24,16 @@ class NetworkAnalyzer(object):
         else:
             logger.info('Connection established.')
 
-    def run(self, port) -> list:
-        """Measure S-Parameters for [port]"""
-        while True:
-            # Measure S-Parameters for [ports]
-            # Sweep timing handled automatically
-            # Returns result:
-            # 3-Dimensional numpy.ndarray:
-            # [freq_point][output_port-1][input_port-1]
-            array: list = self.vna.channel(1).measure([1,2])
+    def run(self, input_port = 1) -> None:
+        """Measure S-Parameters for [input port]"""
+        # Sweep timing handled automatically
+        # Returns result:
+        # 3-Dimensional numpy.ndarray:
+        # [freq_point][input_port-1]
+        array: numpy.ndarray = self.vna.channel(1).measure(input_port)
+        array.max(axis=1)
+        
+
         
     def stop(self) -> None:
         """Close connection."""
